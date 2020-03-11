600baf9b7d261db100c93917aa7807a05ac3daa5
wesleyol <Wesley.ol@gmail.com>
2020-03-10 22:17:45 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,11 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n===== TESTE 3: seller findById =====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
-
 }
